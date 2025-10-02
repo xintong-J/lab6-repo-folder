da0fab7cc92c7051d042ee94825876d908a5ddcf
Xintong Jiang <231744781+xintong-J@users.noreply.github.com>
2025-10-02 21:33:56 +0000

    changes in the file

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -4,15 +4,15 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
     # prepare input values
-    text = "Bond"
+    text = "My name is Bond."
     start = 11
     end = 15
     # call `sample_run_anonymizer`
-    result = sample_run_anonymizer(text, start, end)
+    #sample_run_anonymizer("Bond", 11, 15)
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
     #result =  sample_run_anonymizer(text,start,end)
     # check the output of the call
     assert result.text == "My name is BIP."
     assert len(result.items) == 1
-    #assert result.items[0]
     assert result.items[0].start == 11
     assert result.items[0].end == 14
