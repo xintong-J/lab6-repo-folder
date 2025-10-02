ae2d35928ee8bd3aaa0337013637c430f5202c74
Xintong Jiang <231744781+xintong-J@users.noreply.github.com>
2025-10-02 16:50:20 +0000

    Changes for input

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,7 +1,7 @@
 from presidio_anonymizer import AnonymizerEngine
-from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
+from presidio_anonymizer.entities import RecognizerResult, OperatorConfig, EngineResult
 
-def sample_run_anonymizer():
+def sample_run_anonymizer(text:str, start:int, end:int):
     # Initialize the engine
     engine = AnonymizerEngine()
 
@@ -9,12 +9,12 @@ def sample_run_anonymizer():
     # analyzer results (potentially coming from presidio-analyzer) and
     # Operators to get the anonymization output:
     result = engine.anonymize(
-        text=input("text: "),
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=int(input("start: ")), end=int(input("end: ")), score=0.8)],
+        text=text,
+        analyzer_results=[RecognizerResult(entity_type="PERSON", start=start, end=end, score=0.8)],
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
 
-    print(result)
+    return result
 
     # input should be:
     # text: My name is Bond.
@@ -29,4 +29,5 @@ def sample_run_anonymizer():
     # ]
 
 if __name__ == "__main__": 
-    sample_run_anonymizer();
+    result = sample_run_anonymizer("My name is Bond.",11,15);
+    print(result)
